6dad5d609335a9182e384b1d4b176cfbce17114d
Nick Bernard & Nuatu Tseggai <student@epicodus.com>
2014-07-22 14:57:15 -0700

    returns an allergen that corresponds to the inputted score

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -1,18 +1,16 @@
 def allergies(input)
   allergens = {"eggs" => 1, "peanuts" => 2, "shellfish" => 4, "strawberries" => 8, "tomatoes" => 16, "chocolate" => 32, "pollen" => 64, "cats" => 128}
-  inputs = input.split(" ")
-  score = 0
+  # inputs = input.split(" ")
+  score = input
+  result = []
 
-  inputs.each do |x|
-    allergens.each do |y|
-      # puts y[0]
-      if x == y[0]
-        score += y[1]
-      end
+  allergens.each do |allergen, allergen_score|
+    if allergen_score <= score
+      result.push(allergen)
+      score -= allergen_score
     end
   end
-  puts score
-  score
+  result
 end
 
-allergies("eggs shellfish")
+puts allergies(3)
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -3,10 +3,6 @@ require('allergies')
 
 describe('allergies') do
   it("returns a score of 1 if the user is allergic to eggs") do
-    allergies("eggs").should(eq(1))
-  end
-
-  it("returns a score of 1 if the user is allergic to eggs") do
-    allergies("eggs peanuts").should(eq(3))
+    allergies(1).should(eq(["eggs"]))
   end
 end
